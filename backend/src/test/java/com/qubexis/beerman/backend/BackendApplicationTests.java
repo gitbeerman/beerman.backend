1edbb379e67921d3a87a5b5b058a297cd6f6904f
gitbeerman <96922258+gitbeerman@users.noreply.github.com>
2022-01-15 16:04:21 -0400

    Add files via upload
    Initial commit.

---
+++ backend/src/test/java/com/qubexis/beerman/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qubexis.beerman.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
